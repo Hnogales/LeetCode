f1601d55962f3877c149937c5b47024225248b10
Hnogales <hdn2388@gmail.com>
2024-04-16 05:20:52 -0400

    Add Two Integers

---
+++ Easy/Add Two Integers.py
@@ -0,0 +1,17 @@
+'''
+Given two integers num1 and num2, return the sum of the two integers.
+'''
+
+class Solution(object):
+    def sum(self, num1, num2):
+       return num1 + num2
+
+
+sol = Solution()
+
+num1 = 12
+num2 = 5
+
+print(sol.sum(num1, num2))
+
+
